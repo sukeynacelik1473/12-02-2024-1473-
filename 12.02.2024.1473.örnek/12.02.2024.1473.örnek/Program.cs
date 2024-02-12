6053fbdec96e7063496ab0f7b66e911528ab787b
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2024-02-12 11:20:36 +0300

    Add files via upload

---
+++ 12.02.2024.1473.örnek/12.02.2024.1473.örnek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12._02._2024._1473.örnek
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
